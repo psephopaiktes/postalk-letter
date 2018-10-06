095b3279c179fbd2bc0c119bcf65aa217fda8be7
dulltz <isrgnoe@gmail.com>
2018-10-07 08:45:27 +0900

    fix attrs

--- letter.rb
+++ letter.rb
@@ -6,10 +6,11 @@ end
 
 get '/letter_front' do
   @letter = Letter.new
-  @letter.postal_code = params['postal_code']
   @letter.message = params['message']
+  @letter.receiver_postal_code = params['receiver_postal_code']
   @letter.receiver_name = params['receiver_name']
   @letter.receiver_address = params['receiver_address']
+  @letter.sender_postal_code = params['sender_postal_code']
   @letter.sender_name = params['sender_name']
   @letter.sender_address = params['sender_address']
   erb :letter_front
@@ -18,15 +19,15 @@ end
 get '/letter_back' do
   @letter = Letter.new
   @letter.message = params['message']
-  @letter.postal_code = params['postal_code']
-  @letter.message = params['message']
+  @letter.receiver_postal_code = params['receiver_postal_code']
   @letter.receiver_name = params['receiver_name']
   @letter.receiver_address = params['receiver_address']
+  @letter.sender_postal_code = params['sender_postal_code']
   @letter.sender_name = params['sender_name']
   @letter.sender_address = params['sender_address']
   erb :letter_back
 end
 
 class Letter
-  attr_accessor :postal_code, :message, :receiver_name, :receiver_address, :sender_name, :sender_address
+  attr_accessor :message, :receiver_postal_code, :receiver_name, :receiver_address, :sender_postal_code, :sender_name, :sender_address
 end
